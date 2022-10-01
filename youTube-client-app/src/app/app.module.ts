65f1015369c2a9dac75159ba74e83ef57b1dd55c
Oksana Astapova <astapova.frontend@gmail.com>
2022-10-01 23:29:45 +0300

    feat: create components

--- youTube-client-app/src/app/app.module.ts
+++ youTube-client-app/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SortComponent } from './sort/sort.component';
+import { CardsComponent } from './cards/cards.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { CreateCardComponent } from './create-card/create-card.component';
+import { MoreInfoComponent } from './more-info/more-info.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { SearchItemComponent } from './search-item/search-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortComponent,
+    CardsComponent,
+    AuthorizationComponent,
+    CreateCardComponent,
+    MoreInfoComponent,
+    SearchResultComponent,
+    SearchItemComponent
   ],
   imports: [
     BrowserModule,
